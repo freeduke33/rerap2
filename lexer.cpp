f7e0c409acc489c71435f9b7b06f494e5d146673
Alex Kapranoff <alex@kapranoff.ru>
2020-12-23 13:40:46 -0800

    Bring back support for original "end" keyword

--- lexer.cpp
+++ lexer.cpp
@@ -131,6 +131,8 @@ Token Lexer::getNextToken()
 	checkKeyword(nextToken, "при", T_WHEN);
 	checkKeyword(nextToken, "while", T_WHILE);
 	checkKeyword(nextToken, "пока", T_WHILE);
+	checkKeyword(nextToken, "все", T_G_END);
+	checkKeyword(nextToken, "всё", T_G_END);
 
 	checkKeyword(nextToken, "yes", T_YES);
 	checkKeyword(nextToken, "да", T_YES);
