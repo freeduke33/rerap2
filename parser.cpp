d6bf537f0b9ab95537a1663ce16b061245ce3522
Alex Kapranoff <alex@kapranoff.ru>
2021-01-09 00:11:26 -0800

    Revert "Support "done/всё" in all compound statements"
    This reverts commit 3a9708e38ca10e3bc1de5b8ac603b57b4bc35796.

--- parser.cpp
+++ parser.cpp
@@ -476,7 +476,7 @@ For* Parser::parseFor()
 
 	// Add commands until "od" or "until" is found
 	std::unique_ptr<NodeList> stmts(new NodeList());
-	while(hasTokens() && peekToken().getType() != T_OD && peekToken().getType() != T_UNTIL && peekToken().getType() != T_G_END)
+	while(hasTokens() && peekToken().getType() != T_OD && peekToken().getType() != T_UNTIL)
 	{
 		stmts->pushNode(parseStatement());
 		parseSeparation();
@@ -488,7 +488,7 @@ For* Parser::parseFor()
 		getToken();
 		result->setUntilCondition(parseExpression());
 	}
-	else if(hasTokens() && (peekToken().getType() == T_OD || peekToken().getType() == T_G_END))
+	else if(hasTokens() && peekToken().getType() == T_OD)
 		getToken();
 	else
 		throw ParserSyntaxException(getToken(), "Expected \"od\" or \"until\" command!");
@@ -908,14 +908,14 @@ Repeat* Parser::parseRepeat()
 
 	// Add commands until "od" or "until" is found
 	std::unique_ptr<NodeList> stmts(new NodeList());
-	while(hasTokens() && !(peekToken().getType() == T_OD || peekToken().getType() == T_UNTIL || peekToken().getType() == T_G_END))
+	while(hasTokens() && !(peekToken().getType() == T_OD || peekToken().getType() == T_UNTIL))
 	{
 		stmts->pushNode(parseStatement());
 		parseSeparation();
 	}
 	result->setStatements(stmts.release());
 
-	if(hasTokens() && (peekToken().getType() == T_OD || peekToken().getType() == T_G_END))
+	if(hasTokens() && peekToken().getType() == T_OD)
 		getToken();
 	else if(hasTokens() && peekToken().getType() == T_UNTIL)
 	{
@@ -1019,7 +1019,7 @@ While* Parser::parseWhile()
 
 	// Add commands until "od" or "until" is found
 	std::unique_ptr<NodeList> whileStmts(new NodeList());
-	while(hasTokens() && !(peekToken().getType() == T_OD || peekToken().getType() == T_UNTIL || peekToken().getType() == T_G_END))
+	while(hasTokens() && !(peekToken().getType() == T_OD || peekToken().getType() == T_UNTIL))
 	{
 		whileStmts->pushNode(parseStatement());
 		parseSeparation();
@@ -1029,7 +1029,7 @@ While* Parser::parseWhile()
 	result->setLineNumber(tok.getLineNumber());
 	result->setColumnNumber(tok.getColumnNumber());
 
-	if(hasTokens() && (peekToken().getType() == T_OD || peekToken().getType() == T_G_END))
+	if(hasTokens() && peekToken().getType() == T_OD)
 	{
 		getToken();
 		result->setCondition(cond.release());
